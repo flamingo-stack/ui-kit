08544024e55e4cc4f3e42c9f616fb52a36d21886
Michael Assraf <michael@flamingo.cx>
2025-07-27 15:50:35 -0400

    fx

--- src/components/index.ts
+++ src/components/index.ts
@@ -13,3 +13,19 @@ export * from './join-waitlist-cta'
 
 // Loading components
 export * from './loading'
+
+// Priority UI components that exist in main components directory
+export * from './avatar'
+export * from './form'
+export * from './command'
+export * from './carousel'
+export * from './drawer'
+export * from './sidebar'
+export * from './resizable'
+export * from './toggle-group'
+export * from './input-otp'
+export * from './collapsible'
+export * from './context-menu'
+export * from './hover-card'
+export * from './table'
+export * from './popover'
