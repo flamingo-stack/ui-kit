ae11c177a02584414880d42857013c4588293b65
Michael Assraf <michael@flamingo.cx>
2025-07-28 00:57:41 -0400

    fix

--- src/components/features/index.ts
+++ src/components/features/index.ts
@@ -2,3 +2,4 @@
 export * from './sso-modal'
 export * from './error-boundary'
 export * from './loading-provider'
+export { DynamicThemeProvider, useDynamicTheme } from '../providers/dynamic-theme-provider'
