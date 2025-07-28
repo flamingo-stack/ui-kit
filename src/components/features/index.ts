4fee23fdd79ad24062bbfa6e21c9c01995d3fa2b
Michael Assraf <michael@flamingo.cx>
2025-07-28 17:16:16 -0400

    fix

--- src/components/features/index.ts
+++ src/components/features/index.ts
@@ -1,6 +1,5 @@
 // Feature Components exports
 export * from './sso-modal'
-export * from './auth-modal'
 export * from './provider-button'
 export * from './auth-trigger'
 export * from './auth-providers-list'
